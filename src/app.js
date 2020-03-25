462186a3c3e63d858c6eeb874796afde04da4d36
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-03-24 21:54:28 -0300

    Implementação das classes e estruturas de base para o OMR

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import config from './config/config'
+import setRoutes from './middlewares/routes'
 
 const app = express()
 
@@ -8,5 +9,7 @@ app.config = config
 
 app.set('port', 8080)
 app.use(bodyParser.json())
+
+setRoutes(app)
 
 export default app
---
+++ src/classes/controller.js
@@ -0,0 +1,40 @@
+import HttpStatus from 'http-status'
+import { defaultResponse, errorResponse } from './Responses'
+
+class ControllerBase {
+    constructor(Model) {
+        this.Model = Model
+    }
+
+    getAll(where = {}) {
+        return this.Model.findAndCountAll ({ where })
+            .then(result => defaultResponse(result.rows, HttpStatus.OK, result.count))
+            .catch(error => errorResponse(error.message))
+    }
+
+    getById(params) {
+        return this.Model.findOne({ where: params })
+            .then(result => defaultResponse(result))
+            .catch(error => errorResponse(error.message))
+    }
+
+    create(data) {
+        return this.Model.create(data)
+            .then(result => defaultResponse(result, HttpStatus.CREATED))
+            .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY))
+    }
+
+    update(data, params) {
+        return this.Model.update(data, { where: params })
+            .then(result => defaultResponse(result))
+            .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY))
+    }
+
+    delete(params) {
+        return this.Model.destroy({ where: params })
+            .then(result => defaultResponse(result, HttpStatus.NO_CONTENT))
+            .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY))
+    }
+}
+
+export default ControllerBase

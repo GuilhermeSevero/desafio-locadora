2f02ae0723695781e6926931da470a74229e6e25
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-03-24 22:13:10 -0300

    Conexao Banco

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import config from './config/config'
+import datasource from './config/datasource'
 import setRoutes from './middlewares/routes'
 
 const app = express()
 
 app.config = config
+app.datasource = datasource(app)
 
 app.set('port', 8080)
 app.use(bodyParser.json())

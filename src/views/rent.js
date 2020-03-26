8cffa0106344e247697e94979bca3c6e07f476e8
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-03-26 20:49:01 -0300

    endpoint de algueis

---
+++ src/views/rent.js
@@ -0,0 +1,9 @@
+import setDefaultRoutes from '../classes/routes'
+import RentController from '../controllers/rent'
+
+
+export default (app) => {
+    const rentController = new RentController(app.datasource.models.rent)
+
+    setDefaultRoutes(app, rentController, 'alugueis')
+}

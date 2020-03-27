7a5d2d5f54b53159e0f1bd077017ad5aa0a04c6a
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-03-26 23:50:48 -0300

    Alugar

--- src/views/movie.js
+++ src/views/movie.js
@@ -5,32 +5,21 @@ import HttpStatus from 'http-status'
 
 
 export default (app) => {
-    const movieController = new MovieController(app.datasource.models, 'movie')
+    const movieController = new MovieController(app.datasource.models.movie)
 
     setDefaultRoutes(app, movieController, 'filmes')
 
     app.route(`/filmes/:id/alugar`)
         .all(app.auth)
         .post((req, res) => {
-            movieController.rent(req.params.id, req.userId)
+            if (!req.body.hasOwnProperty('userId')) {
+                res.status(HttpStatus.BAD_REQUEST)
+                res.json('Parâmetro "userId" é obrigatório!')
+            }
+            movieController.rent(req.params.id, req.body.userId)
                 .then(result => {
                     res.status(result.statusCode)
                     res.json(result.data)
                 })
         })
-
-        app.route(`/filmes/:id/devolver`)
-            .all(app.auth)
-            .post((req, res) => {
-                if (!req.body.hasOwnProperty('amount')) {
-                    res.status(HttpStatus.BAD_REQUEST)
-                    res.json({ erro: 'Parâmetro "amount" é obrigatório!' })
-                } else {
-                    movieController.rent(req.params.id, req.userId, req.body.amount)
-                        .then(result => {
-                            res.status(result.statusCode)
-                            res.json(result.data)
-                        })
-                }
-            })
 }

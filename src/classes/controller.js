616cc0d4579cf58be4e78889dd52207f46bb1995
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-03-26 21:58:06 -0300

    Ajuste nome para camelcase

--- src/classes/controller.js
+++ src/classes/controller.js
@@ -2,36 +2,36 @@ import HttpStatus from 'http-status'
 import { defaultResponse, errorResponse } from './responses'
 
 class ControllerBase {
-    constructor(Model) {
-        this.Model = Model
+    constructor(model) {
+        this.model = model
     }
 
     getAll(where = {}) {
-        return this.Model.findAndCountAll ({ where })
+        return this.model.findAndCountAll ({ where })
             .then(result => defaultResponse(result.rows, HttpStatus.OK, result.count))
             .catch(error => errorResponse(error.message))
     }
 
     getById(params) {
-        return this.Model.findOne({ where: params })
+        return this.model.findOne({ where: params })
             .then(result => defaultResponse(result))
             .catch(error => errorResponse(error.message))
     }
 
     create(data) {
-        return this.Model.create(data)
+        return this.model.create(data)
             .then(result => defaultResponse(result, HttpStatus.CREATED))
             .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY))
     }
 
     update(data, params) {
-        return this.Model.update(data, { where: params })
+        return this.model.update(data, { where: params })
             .then(result => defaultResponse(result))
             .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY))
     }
 
     delete(params) {
-        return this.Model.destroy({ where: params })
+        return this.model.destroy({ where: params })
             .then(result => defaultResponse(result, HttpStatus.NO_CONTENT))
             .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY))
     }
